ab7c1e3e1952c5b88ccffd508a6972bf024d30c7
HDRanasingha <138963111+HDRanasingha@users.noreply.github.com>
2025-02-15 23:04:33 +0530

    Growers order page stylled.

--- client/src/pages/GrowersOrderPage.jsx
+++ client/src/pages/GrowersOrderPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Navbar from "../component/Navbar";
 import Footer from "../component/Footer";
+import "../styles/GrowersOrders.scss";
 
 const GrowersOrderPage = () => {
   const [orders, setOrders] = useState([]);
